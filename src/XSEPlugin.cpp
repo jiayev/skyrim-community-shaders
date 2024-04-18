b0b70ea39acbc9f9da41c5864b2b49c4a45e37c5
Ilya Perapechka <50655277+Jonahex@users.noreply.github.com>
2024-04-18 06:36:40 +0300

    feat: add vanilla frame annotations (#273)

--- src/XSEPlugin.cpp
+++ src/XSEPlugin.cpp
@@ -1,5 +1,6 @@
 #include "Hooks.h"
 
+#include "FrameAnnotations.h"
 #include "Menu.h"
 #include "ShaderCache.h"
 #include "State.h"
@@ -91,6 +92,7 @@ void MessageHandler(SKSE::MessagingInterface::Message* message)
 			if (errors.empty()) {
 				State::GetSingleton()->PostPostLoad();
 				Hooks::Install();
+				FrameAnnotations::OnPostPostLoad();
 
 				auto& shaderCache = SIE::ShaderCache::Instance();
 
@@ -114,6 +116,8 @@ void MessageHandler(SKSE::MessagingInterface::Message* message)
 			}
 
 			if (errors.empty()) {
+				FrameAnnotations::OnDataLoaded();
+
 				auto& shaderCache = SIE::ShaderCache::Instance();
 				shaderCache.menuLoaded = true;
 				while (shaderCache.IsCompiling() && !shaderCache.backgroundCompilation) {
