ccf7275eca756cf268e43c41eac32a3f720984aa
Pentalimbed <pentalimbed@gmail.com>
2024-08-04 23:39:17 +0100

    feat: port COD bloom

--- src/Features/PostProcessing/PostProcessFeature.cpp
+++ src/Features/PostProcessing/PostProcessFeature.cpp
@@ -1,9 +1,12 @@
 #include "PostProcessFeature.h"
 
+#include "CODBloom.h"
+
 const ankerl::unordered_dense::map<std::string, PostProcessFeatureConstructor>& PostProcessFeatureConstructor::GetFeatureConstructors()
 {
 	static ankerl::unordered_dense::map<std::string, PostProcessFeatureConstructor> retval = {
 		GetFeatureConstructorPair<TestFeature>(),
+		GetFeatureConstructorPair<CODBloom>(),
 	};
 	return retval;
 }
