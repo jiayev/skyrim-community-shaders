552d3204a2b8d93e7ed66306b07d11f16122f6f6
doodlum <doodlum@users.noreply.github.com>
2025-01-14 18:26:52 +0000

    style: 🎨 apply clang-format changes

--- src/Util.h
+++ src/Util.h
@@ -1,11 +1,10 @@
 #pragma once
 
 #include "Utils/D3D.h"
+#include "Utils/Form.h"
 #include "Utils/Format.h"
 #include "Utils/Game.h"
 #include "Utils/GameSetting.h"
 #include "Utils/Serialize.h"
 #include "Utils/UI.h"
 #include "Utils/WinApi.h"
-#include "Utils/Form.h"
-
