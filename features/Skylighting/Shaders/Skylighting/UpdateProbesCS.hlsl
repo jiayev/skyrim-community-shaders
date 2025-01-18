13043a076fa4c6f7c38d9e85423dbb01474fb049
doodlum <15017472+doodlum@users.noreply.github.com>
2025-01-18 18:26:58 +0000

    fix: skylighting fixes (#898)
    * fix: skylighting fixes
    
    * style: 🎨 apply clang-format changes
    
    * fix: pause skylighting on map menu
    
    * feat: noise for skylighting + better ssr to match it
    
    * style: 🎨 apply clang-format changes
    
    * chore: allow animated
    
    * chore: only use noise with taa on
    
    * chore: adjust minimum specular and diffuse
    
    * style: 🎨 apply clang-format changes
    
    * chore: increase minimum radius
    
    * chore: cleanup
    
    * feat: ssgi dimmer
    
    * chore: bump versions
    
    * style: 🎨 apply clang-format changes
    
    * chore: maybe ok description
    
    * fix: no articulated
    
    * fix: a bit better glaciers
    
    * style: 🎨 apply clang-format changes
    
    * chore: tweak ssgi dimmer
    
    * chore: more cleanup and fixing
    
    * style: 🎨 apply clang-format changes
    
    * fix: receiver plane bias
    
    * style: 🎨 apply clang-format changes
    
    * chore: call normal bias i guess
    
    * fix: sort of better ssr fade
    
    * style: 🎨 apply clang-format changes
    
    ---------
    
    Co-authored-by: doodlum <doodlum@users.noreply.github.com>

--- features/Skylighting/Shaders/Skylighting/UpdateProbesCS.hlsl
+++ features/Skylighting/Shaders/Skylighting/UpdateProbesCS.hlsl
@@ -6,11 +6,11 @@ Texture2D<unorm float> srcOcclusionDepth : register(t0);
 RWTexture3D<sh2> outProbeArray : register(u0);
 RWTexture3D<uint> outAccumFramesArray : register(u1);
 
-SamplerState samplerPointClamp : register(s0);
+SamplerComparisonState comparisonSampler : register(s0);
 
 [numthreads(8, 8, 1)] void main(uint3 dtid
 								: SV_DispatchThreadID) {
-	const float fadeInThreshold = 255;
+	const float fadeInThreshold = 15;
 	const static sh2 unitSH = float4(sqrt(4.0 * Math::PI), 0, 0, 0);
 	const SharedData::SkylightingSettings settings = SharedData::skylightingSettings;
 
@@ -26,23 +26,21 @@ SamplerState samplerPointClamp : register(s0);
 
 	if (all(occlusionUV > 0) && all(occlusionUV < 1)) {
 		uint accumFrames = isValid ? (outAccumFramesArray[dtid] + 1) : 1;
-		if (accumFrames < fadeInThreshold) {
-			float occlusionDepth = srcOcclusionDepth.SampleLevel(samplerPointClamp, occlusionUV, 0);
-			float visibility = saturate((occlusionDepth + 0.0005 - cellCentreOS.z) * 1024);
+		float occlusionDepth = srcOcclusionDepth.SampleCmpLevelZero(comparisonSampler, occlusionUV, 0);
+		float visibility = srcOcclusionDepth.SampleCmpLevelZero(comparisonSampler, occlusionUV, cellCentreOS.z);
 
-			sh2 occlusionSH = SphericalHarmonics::Scale(SphericalHarmonics::Evaluate(settings.OcclusionDir.xyz), visibility * 4.0 * Math::PI);  // 4 pi from monte carlo
-			if (isValid) {
-				float lerpFactor = rcp(accumFrames);
-				sh2 prevProbeSH = unitSH;
-				if (accumFrames > 1)
-					prevProbeSH += (outProbeArray[dtid] - unitSH) * fadeInThreshold / min(fadeInThreshold, accumFrames - 1);  // inverse confidence
-				occlusionSH = SphericalHarmonics::Add(SphericalHarmonics::Scale(prevProbeSH, 1 - lerpFactor), SphericalHarmonics::Scale(occlusionSH, lerpFactor));
-			}
-			occlusionSH = lerp(unitSH, occlusionSH, min(fadeInThreshold, accumFrames) / fadeInThreshold);  // confidence fade in
-
-			outProbeArray[dtid] = occlusionSH;
-			outAccumFramesArray[dtid] = accumFrames;
+		sh2 occlusionSH = SphericalHarmonics::Scale(SphericalHarmonics::Evaluate(settings.OcclusionDir.xyz), visibility * 4.0 * Math::PI);  // 4 pi from monte carlo
+		if (isValid) {
+			float lerpFactor = rcp(accumFrames);
+			sh2 prevProbeSH = unitSH;
+			if (accumFrames > 1)
+				prevProbeSH += (outProbeArray[dtid] - unitSH) * fadeInThreshold / min(fadeInThreshold, accumFrames - 1);  // inverse confidence
+			occlusionSH = lerp(prevProbeSH, occlusionSH, lerpFactor);
 		}
+		occlusionSH = lerp(unitSH, occlusionSH, min(fadeInThreshold, accumFrames) / fadeInThreshold);  // confidence fade in
+
+		outProbeArray[dtid] = occlusionSH;
+		outAccumFramesArray[dtid] = accumFrames;
 	} else if (!isValid) {
 		outProbeArray[dtid] = unitSH;
 		outAccumFramesArray[dtid] = 0;
