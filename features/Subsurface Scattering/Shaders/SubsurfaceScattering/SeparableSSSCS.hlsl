feb30e9b5a558889a868ad6781565feb0a279aee
刘育嘉 <l936249247@hotmail.com>
2025-03-20 15:22:16 +0800

    using diffuse irradiance to do ssss

--- features/Subsurface Scattering/Shaders/SubsurfaceScattering/SeparableSSSCS.hlsl
+++ features/Subsurface Scattering/Shaders/SubsurfaceScattering/SeparableSSSCS.hlsl
@@ -51,6 +51,7 @@ cbuffer PerFrameSSS : register(b1)
 		bool humanProfile = MaskTexture[DTid.xy].y > 0.0;
 
 		float4 color = SSSSBlurCS(DTid.xy, texCoord, float2(0.0, 1.0), sssAmount, humanProfile);
+		color.rgb *= AlbedoTexture[DTid.xy].rgb;
 		color.rgb = Color::LinearToGamma(color.rgb);
 		SSSRW[DTid.xy] = float4(color.rgb, 1.0);
 	}
