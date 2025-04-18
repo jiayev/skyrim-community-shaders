76c40b20a82f84ebb4b6e1be3ce365bf238a6cc8
刘育嘉 <l936249247@hotmail.com>
2025-04-18 18:00:42 +0800

    ISHDR skip

--- package/Shaders/ISHDR.hlsl
+++ package/Shaders/ISHDR.hlsl
@@ -91,6 +91,11 @@ PS_OUTPUT main(PS_INPUT input)
 
 	float3 inputColor = BlendTex.Sample(BlendSampler, uv).xyz;
 
+#		if defined(POSTPROCESS)
+	psout.Color = float4(inputColor, 1.0);
+	return psout;
+#		endif
+
 	float3 bloomColor = 0;
 	if (Flags.x > 0.5) {
 		bloomColor = ImageTex.Sample(ImageSampler, uv).xyz;
