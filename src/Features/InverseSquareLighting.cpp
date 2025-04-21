5ecf23c248dcb2a6af2adb62d1f1a91970cbc518
刘育嘉 <l936249247@hotmail.com>
2025-04-21 13:43:10 +0800

    fix isl brightness

--- src/Features/InverseSquareLighting.cpp
+++ src/Features/InverseSquareLighting.cpp
@@ -60,8 +60,8 @@ void InverseSquareLighting::ProcessLight(LightLimitFix::LightData& light, RE::BS
 		runtimeData->radius.y = light.radius;
 		runtimeData->radius.z = light.radius;
 		light.color /= std::max(0.001f, std::max(light.color.x, std::max(light.color.y, light.color.z)));
-		light.color *= intensity;
-		light.fade = 1.0f;
+		// light.color *= intensity;
+		light.fade = intensity;
 	} else {
 		light.radius = runtimeData->radius.x;
 		light.fade = runtimeData->fade;
