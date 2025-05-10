b208eca54c83aa70b3079c7488264f89b9352626
刘育嘉 <l936249247@hotmail.com>
2025-05-10 19:53:47 +0800

    initial commit wip

---
+++ features/Screen Space Point Light Shadows/Shaders/ScreenSpacePointLightShadows/SSPLS.hlsli
@@ -0,0 +1,7 @@
+#ifndef SSPLS_COMMON
+#define SSPLS_COMMON
+
+namespace ScreenSpacePointLightShadows
+{
+    Texture2D<float4> SSPLSTexture : register(t56);
+}
--- src/Globals.h
+++ src/Globals.h
@@ -12,6 +12,7 @@ struct LODBlending;
 struct InteriorSunShadows;
 struct InverseSquareLighting;
 struct ScreenSpaceGI;
+struct ScreenSpacePointLightShadows;
 struct ScreenSpaceShadows;
 struct Skylighting;
 struct TerrainVariation;
@@ -64,6 +65,7 @@ namespace globals
 		extern InteriorSunShadows* interiorSunShadows;
 		extern InverseSquareLighting* inverseSquareLighting;
 		extern ScreenSpaceGI* screenSpaceGI;
+		extern ScreenSpacePointLightShadows* screenSpacePointLightShadows;
 		extern ScreenSpaceShadows* screenSpaceShadows;
 		extern Skylighting* skylighting;
 		extern TerrainVariation* terrainVariation;
