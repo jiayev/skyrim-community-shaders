2b85cbc5d3399d99485682c1d139eb10b8ac9b72
刘育嘉 <l936249247@hotmail.com>
2025-05-18 03:16:15 +0800

    enable for vr

--- src/Features/ScreenSpacePointLightShadows.h
+++ src/Features/ScreenSpacePointLightShadows.h
@@ -84,5 +84,7 @@ struct ScreenSpacePointLightShadows : Feature
 	virtual void SaveSettings(json& o_json) override;
     virtual void RestoreDefaultSettings() override;
 
+    virtual bool SupportsVR() override { return true; };
+
     PerFrame GetCommonBufferData();
 };
