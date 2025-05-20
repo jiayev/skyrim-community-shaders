f2a416040912b99bc10bb9c00a23428c6ff46d0c
刘育嘉 <l936249247@hotmail.com>
2025-05-21 02:03:34 +0800

    fix: correct syntax error in Prepass function

--- src/Features/XeGTAO.cpp
+++ src/Features/XeGTAO.cpp
@@ -177,8 +177,8 @@ void XeGTAOFeature::Prepass()
 	}
 
 	{
-		ID3D11UnorderedAccessView* uavs[1] { nullptr }
-		； context->CSSetUnorderedAccessViews(0, 1, uavs, nullptr);
+		ID3D11UnorderedAccessView* uavs[1] { nullptr };
+		context->CSSetUnorderedAccessViews(0, 1, uavs, nullptr);
 
 		ID3D11ShaderResourceView* srvs[1]{ nullptr };
 		context->CSSetShaderResources(0, 1, srvs);
