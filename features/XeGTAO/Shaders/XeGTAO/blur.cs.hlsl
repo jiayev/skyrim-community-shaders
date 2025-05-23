1986781e17d1c48cf3de4a557887d052c86dda63
jiayev <jiayev@users.noreply.github.com>
2025-05-23 05:27:05 +0000

    style: 🎨 apply clang-format changes

--- features/XeGTAO/Shaders/XeGTAO/blur.cs.hlsl
+++ features/XeGTAO/Shaders/XeGTAO/blur.cs.hlsl
@@ -10,9 +10,9 @@ RWTexture2D<uint> outBlurred : register(u0);
 SamplerState linearSampler : register(s0);
 
 [numthreads(XE_GTAO_NUMTHREADS_X, XE_GTAO_NUMTHREADS_Y, 1)] void main(const uint2 dtid
-								: SV_DispatchThreadID) {
-    float invResX = FrameBuffer::DynamicResolutionParams2.x;
-    float invResY = FrameBuffer::DynamicResolutionParams2.y;
+																	  : SV_DispatchThreadID) {
+	float invResX = FrameBuffer::DynamicResolutionParams2.x;
+	float invResY = FrameBuffer::DynamicResolutionParams2.y;
 
 	float2 texCoord = (dtid + 0.5) * float2(invResX, invResY);
 
@@ -27,9 +27,9 @@ SamplerState linearSampler : register(s0);
 	for (int x = -radius; x < (radius + 1); x++) {
 		for (int y = -radius; y < (radius + 1); y++) {
 			float2 sample_tex_coord = clamp(texCoord.xy + float2(x, y) * float2(invResX, invResY), 0, 1);
-            uint2 sample_pixel_coord = dtid + uint2(x, y);
+			uint2 sample_pixel_coord = dtid + uint2(x, y);
 			uint data_sample_packed = texNormal.Load(sample_pixel_coord).x;
-            float3 data_sample = XeGTAO_R11G11B10_UNORM_to_FLOAT3(data_sample_unpacked);
+			float3 data_sample = XeGTAO_R11G11B10_UNORM_to_FLOAT3(data_sample_unpacked);
 
 			float blur_sample_view_depth = SharedData::GetScreenDepth(sample_tex_coord);
 
@@ -41,7 +41,7 @@ SamplerState linearSampler : register(s0);
 		}
 	}
 
-    float3 output = sum_data / sum_weight;
+	float3 output = sum_data / sum_weight;
 
 	outBlurred[dtid] = XeGTAO_FLOAT3_to_R11G11B10_UNORM(output);
 }
