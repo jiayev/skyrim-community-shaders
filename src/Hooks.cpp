e9f23093ae036b4e82022bb700e7545664b8d35a
刘育嘉 <l936249247@hotmail.com>
2025-05-27 11:56:12 +0800

    initial

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -11,6 +11,7 @@
 
 #include "Features/InteriorSunShadows.h"
 #include "Features/LightLimitFix.h"
+#include "Features/Skin.h"
 #include "Features/TerrainHelper.h"
 
 #include "ShaderTools/BSShaderHooks.h"
@@ -844,6 +845,12 @@ namespace Hooks
 			auto terrainHelper = globals::features::terrainHelper;
 			if (terrainHelper->loaded) {
 				terrainHelper->BSLightingShader_SetupMaterial(material);
+			}
+
+			// advanced skin
+			auto skin = globals::features::skin;
+			if (skin->loaded) {
+				skin->BSLightingShader_SetupMaterial(material);
 			}
 		};
 		static inline REL::Relocation<decltype(thunk)> func;
