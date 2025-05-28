66dce514540b223205858fd1a55b0c790a55b696
刘育嘉 <l936249247@hotmail.com>
2025-05-28 18:02:00 +0800

    fix pbrglossiness for wet

--- package/Shaders/Lighting.hlsl
+++ package/Shaders/Lighting.hlsl
@@ -2194,7 +2194,7 @@ PS_OUTPUT main(PS_INPUT input, bool frontFace
 	float3 transmissionColor = 0;
 
 	float pbrWeight = 1;
-	float pbrGlossiness = 1 - skinSurfaceProperties.RoughnessPrimary;
+	float pbrGlossiness = 1 - lerp(lerp(skinSurfaceProperties.RoughnessPrimary, skinSurfaceProperties.RoughnessSecondary, skinSurfaceProperties.SecondarySpecIntensity), 0.1, skinSurfaceProperties.Wetness);
 #	endif  // CS_SKIN
 
 	float porosity = 1.0;
