a36d19ee014ccd217ebc06a04ce7f4c80b276e62
刘育嘉 <l936249247@hotmail.com>
2025-06-18 15:56:24 +0800

    fix: some features not loaded correctly

--- src/Feature.cpp
+++ src/Feature.cpp
@@ -31,6 +31,19 @@
 
 void Feature::Load(json& o_json)
 {
+	if (o_json[GetName()].is_structured()) {
+		logger::info("Loading {} settings", GetName());
+		try {
+			LoadSettings(o_json[GetName()]);
+		} catch (...) {
+			logger::warn("Invalid settings for {}, using default.", GetName());
+			RestoreDefaultSettings();
+		}
+	} else {
+		logger::info("Loading default settings for {}", GetName());
+		RestoreDefaultSettings();
+	}
+
 	// Convert string to wstring
 	auto ini_filename = std::format("{}.ini", GetShortName());
 	std::wstring ini_filename_w;
@@ -135,20 +148,6 @@ void Feature::Load(json& o_json)
 
 		} else {
 			logger::error("Feature has empty short name, cannot add to feature issues list");
-		}
-	} else {
-		// No errors, load settings now
-		if (o_json[GetName()].is_structured()) {
-			logger::info("Loading {} settings", GetName());
-			try {
-				LoadSettings(o_json[GetName()]);
-			} catch (...) {
-				logger::warn("Invalid settings for {}, using default.", GetName());
-				RestoreDefaultSettings();
-			}
-		} else {
-			logger::info("Loading default settings for {}", GetName());
-			RestoreDefaultSettings();
 		}
 	}
 }
