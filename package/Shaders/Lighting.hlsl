f13b8684d4346092337420a9c2a94c91816ff855
刘育嘉 <l936249247@hotmail.com>
2025-08-02 02:56:50 +0800

    fix define

--- package/Shaders/Lighting.hlsl
+++ package/Shaders/Lighting.hlsl
@@ -2925,7 +2925,7 @@ PS_OUTPUT main(PS_INPUT input, bool frontFace : SV_IsFrontFace)
 			reflectance = DynamicCubemaps::GetDynamicCubemap(worldNormal, vertexNormal, viewDirection, roughness, F0);
 #		endif
 
-#		if defined(ENVMAP) || defined(MULTI_LAYER_PARALLAX) || defined(EYE) && !defined(VANILLA_FRESNEL)
+#		if (defined(ENVMAP) || defined(MULTI_LAYER_PARALLAX) || defined(EYE)) && !defined(VANILLA_FRESNEL)
 		reflectance *= envMask;
 #		endif
 	}
