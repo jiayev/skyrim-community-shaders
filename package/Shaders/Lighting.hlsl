b1b0c24a79378880d3f736f2542be703a990e24d
刘育嘉 <l936249247@hotmail.com>
2025-08-01 14:32:10 +0800

    fix complex material

--- package/Shaders/Lighting.hlsl
+++ package/Shaders/Lighting.hlsl
@@ -1901,7 +1901,7 @@ PS_OUTPUT main(PS_INPUT input, bool frontFace : SV_IsFrontFace)
 	complexMaterial = complexMaterial && complexMaterialColor.y > (4.0 / 255.0);
 	shininess = lerp(shininess, shininess * complexMaterialColor.y, complexMaterial);
 	if (complexMaterial) {
-		complexSpecular = lerp(0.04, baseColor.xyz, complexMaterialColor.z);
+		complexSpecular = lerp(1.0, baseColor.xyz, complexMaterialColor.z);
 		baseColor.xyz = lerp(baseColor.xyz, 0.0, complexMaterialColor.z);
 
 	}
@@ -2291,8 +2291,12 @@ PS_OUTPUT main(PS_INPUT input, bool frontFace : SV_IsFrontFace)
 #			endif
 
 				envMask = saturate(envMask);
-#			if defined(SPECULAR)
+#			if defined(VANILLA_FRESNEL)
+#				if defined(SPECULAR)
 				F0 = lerp(glossiness * SpecularColor.xyz, F0, envMask);
+#				else
+				F0 = lerp(0.04, F0, envMask);
+#				endif
 #			endif
 				roughness = lerp(roughness, envRoughness, pow(envMask, 0.25));
 			}
@@ -2920,6 +2924,10 @@ PS_OUTPUT main(PS_INPUT input, bool frontFace : SV_IsFrontFace)
 #		else
 			reflectance = DynamicCubemaps::GetDynamicCubemap(worldNormal, vertexNormal, viewDirection, roughness, F0);
 #		endif
+
+#		if defined(ENVMAP) || defined(MULTI_LAYER_PARALLAX) || defined(EYE) && !defined(VANILLA_FRESNEL)
+		reflectance *= envMask;
+#		endif
 	}
 #	endif
 
@@ -2945,7 +2953,7 @@ PS_OUTPUT main(PS_INPUT input, bool frontFace : SV_IsFrontFace)
 	baseColor.xyz = lerp(baseColor.xyz, pow(abs(baseColor.xyz), 1.0 + wetnessDarkeningAmount), 0.8);
 #		endif
 
-	float3 wetnessReflectance = WetnessEffects::GetWetnessAmbientSpecular(screenUV, wetnessNormal, vertexNormal, viewDirection, waterRoughnessSpecular);
+	float3 wetnessReflectance = WetnessEffects::GetWetnessAmbientSpecular(screenUV, wetnessNormal, vertexNormal, viewDirection, waterRoughnessSpecular) * wetnessGlossinessSpecular;
 
 #		if !defined(DEFERRED)
 	wetnessSpecular += wetnessReflectance;
@@ -3055,15 +3063,12 @@ PS_OUTPUT main(PS_INPUT input, bool frontFace : SV_IsFrontFace)
 	}
 #	else
 	color.xyz += diffuseColor * baseColor.xyz;
-#		if !defined(DEFERRED) && defined(VANILLA_FRESNEL)
-#			if defined(DYNAMIC_CUBEMAPS)
-#				if defined(SKYLIGHTING)
-	specularColor += DynamicCubemaps::GetDynamicCubemapSpecularIrradiance(screenUV, worldNormal, vertexNormal, viewDirection, roughness, skylightingSH) * reflectance;
-#				else
-	specularColor += DynamicCubemaps::GetDynamicCubemapSpecularIrradiance(screenUV, worldNormal, vertexNormal, viewDirection, roughness) * reflectance;
-#				endif
+#		if !defined(DEFERRED) && (defined(ENVMAP) || defined(MULTI_LAYER_PARALLAX) || defined(EYE)) && defined(DYNAMIC_CUBEMAPS)
+	if (dynamicCubemap)
+#			if defined(SKYLIGHTING)
+		envColor += DynamicCubemaps::GetDynamicCubemapSpecularIrradiance(screenUV, worldNormal, vertexNormal, viewDirection, roughness, skylightingSH) * reflectance;
 #			else
-	specularColor += directionalAmbientColor * reflectance;
+		envColor += DynamicCubemaps::GetDynamicCubemapSpecularIrradiance(screenUV, worldNormal, vertexNormal, viewDirection, roughness) * reflectance;
 #			endif
 #		endif
 #	endif
