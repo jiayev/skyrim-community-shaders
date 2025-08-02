a53c3d8b2f57825cfbb4623f07bd4755c44588ca
刘育嘉 <l936249247@hotmail.com>
2025-08-02 14:49:31 +0800

    fix vanilla/cm reflectance

--- package/Shaders/Lighting.hlsl
+++ package/Shaders/Lighting.hlsl
@@ -2297,8 +2297,10 @@ PS_OUTPUT main(PS_INPUT input, bool frontFace : SV_IsFrontFace)
 #				else
 				F0 = lerp(0.04, F0, envMask);
 #				endif
-#			endif
 				roughness = lerp(roughness, envRoughness, pow(envMask, 0.25));
+#			else
+				roughness = envRoughness;
+#			endif
 			}
 		}
 #		endif
@@ -3067,11 +3069,7 @@ PS_OUTPUT main(PS_INPUT input, bool frontFace : SV_IsFrontFace)
 	color.xyz += diffuseColor * baseColor.xyz;
 #		if !defined(DEFERRED) && (defined(ENVMAP) || defined(MULTI_LAYER_PARALLAX) || defined(EYE)) && defined(DYNAMIC_CUBEMAPS)
 	if (dynamicCubemap)
-#			if defined(SKYLIGHTING)
-		envColor += DynamicCubemaps::GetDynamicCubemapSpecularIrradiance(screenUV, worldNormal, vertexNormal, viewDirection, roughness, skylightingSH) * reflectance;
-#			else
-		envColor += DynamicCubemaps::GetDynamicCubemapSpecularIrradiance(screenUV, worldNormal, vertexNormal, viewDirection, roughness) * reflectance;
-#			endif
+		envColor += reflectance;
 #		endif
 #	endif
 
