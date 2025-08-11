aaee3e9eeff5a096fd54521e8b53212e32745214
Tim Butler <8847273+sicsix@users.noreply.github.com>
2025-08-11 17:42:00 +1000

    refactor(interior sun): rename interior sun shadows (#1387)

--- src/XSEPlugin.cpp
+++ src/XSEPlugin.cpp
@@ -148,7 +148,7 @@ void MessageHandler(SKSE::MessagingInterface::Message* message)
 bool Load()
 {
 	if (REL::Module::IsVR()) {
-		REL::IDDatabase::get().IsVRAddressLibraryAtLeastVersion("0.182.0", true);
+		REL::IDDatabase::get().IsVRAddressLibraryAtLeastVersion("0.189.0", true);
 	}
 
 	auto privateProfileRedirectorVersion = Util::GetDllVersion(L"Data/SKSE/Plugins/PrivateProfileRedirector.dll");
