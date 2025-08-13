28dcf5677bf83841133c7094b4fc298410aac369
C. Shen <76898260+Pentalimbed@users.noreply.github.com>
2025-08-13 22:26:20 +0800

    simple ap

--- package/Shaders/Lighting.hlsl
+++ package/Shaders/Lighting.hlsl
@@ -3129,6 +3129,13 @@ PS_OUTPUT main(PS_INPUT input, bool frontFace : SV_IsFrontFace)
 #		endif
 	if (FrameBuffer::FrameParams.y && FrameBuffer::FrameParams.z)
 		color.xyz = lerp(color.xyz, fogColor, input.FogParam.w);
+
+#		if defined(PHYSICAL_SKY)
+	if (SharedData::physSkyData.enabled) {
+		const float4 apSample = PhysSky::SampleAp(normalize(input.WorldPosition.xyz), length(input.WorldPosition.xyz), SampColorSampler);
+		color.xyz = color.xyz * apSample.w + apSample.xyz;
+	}
+#		endif
 #	endif
 
 #	if defined(TESTCUBEMAP) && defined(ENVMAP) && defined(DYNAMIC_CUBEMAPS)
