9a586b5fe4caba89695e494da1ae50ba02a71fd6
jiayev <l936249247@hotmail.com>
2025-08-14 00:15:18 +0800

    add effect blend (#13)

--- package/Shaders/Effect.hlsl
+++ package/Shaders/Effect.hlsl
@@ -872,6 +872,14 @@ PS_OUTPUT main(PS_INPUT input)
 	psout.Color2 = finalColor;
 #	endif
 
+#	if !defined(DEFERRED)
+#		if defined(PHYSICAL_SKY)
+	if (SharedData::physSkyData.enabled) {
+		const float4 apSample = PhysSky::SampleAp(normalize(input.WorldPosition.xyz), length(input.WorldPosition.xyz), SampBaseSampler);
+		psout.Diffuse.xyz = psout.Diffuse.xyz * apSample.w + apSample.xyz;
+	}
+#		endif
+#	endif
 	return psout;
 }
 #endif
