f55c3265d87ad65b81e845805153b9b0a028a61a
刘育嘉 <l936249247@hotmail.com>
2025-09-05 11:16:09 +0800

    1

--- src/JiayeStatement.h
+++ src/JiayeStatement.h
@@ -12,10 +12,10 @@ public:
     void DrawJSInfo() {
         if (ImGui::TreeNodeEx("给中文用户的一些说明")) {
             ImGui::Text("This is some information specifically for Chinese users.");
-            ImGui::Separator();
-            ImGui::Text("2025/09更新：超能阿然为著名小偷奸商，其有著名支持者老江头（目前ID: 吴门老江猪头肉）在B站、QQ群活跃。");
-            ImGui::Text("由于老江头其人多次对奸商的洗白、对mod作者和其他整合作者的诽谤以及对他们成果的剽窃行为，劣迹斑斑，本人在此点名批评。");
-            ImGui::Text("当然，奸商和奸商支持者远不止他们。我们始终欢迎任何形式的举报和曝光。");
+            // ImGui::Separator();
+            // ImGui::Text("2025/09更新：超能阿然为著名小偷奸商，其有著名支持者老江头（目前ID: 吴门老江猪头肉）在B站、QQ群活跃。");
+            // ImGui::Text("由于老江头其人多次对奸商的洗白、对mod作者和其他整合作者的诽谤以及对他们成果的剽窃行为，劣迹斑斑，本人在此点名批评。");
+            // ImGui::Text("当然，奸商和奸商支持者远不止他们。我们始终欢迎任何形式的举报和曝光。");
             ImGui::Separator();
             ImGui::Text("本社区着色器AIO版本是多位开发者共同努力的成果，由Jiaye发布于社区着色器DC服务器。");
             ImGui::Text("在任何其他渠道（除本人亲自发布以外）获取的本社区着色器AIO版本均与本人无关。");
