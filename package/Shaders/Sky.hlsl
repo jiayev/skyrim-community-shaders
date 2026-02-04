d08484aef31c6bf79cf0287df6daf8f076697034
Jiaye <l936249247@hotmail.com>
2026-02-04 20:24:03 +0800

    fix transmittance

--- package/Shaders/Sky.hlsl
+++ package/Shaders/Sky.hlsl
@@ -294,7 +294,7 @@ PS_OUTPUT main(PS_INPUT input)
 
 			const float softEdge = saturate(8.0f * (cosTheta - SharedData::physSkyData.sunDiskCos) / (1.0f - SharedData::physSkyData.sunDiskCos));
 			const float sunSolidAngle = Math::TAU * (1.0f - SharedData::physSkyData.sunDiskCos);
-			const float transmittance = PhysSky::SampleTr(normalize(input.WorldPosition.xyz), SampBlendSampler);
+			const float3 transmittance = PhysSky::SampleTr(normalize(input.WorldPosition.xyz), SampBlendSampler);
 			const float3 sunDiskRadiance = min((SharedData::physSkyData.sunlightColor / max(sunSolidAngle, 1e-6f)) * transmittance, 62250.0f);
 
 			float3 sunDiskColor = sunDiskRadiance * limbFactor * softEdge;
