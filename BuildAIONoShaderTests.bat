7050bd394286b0cfd3602dcb18924c7502cfee58
Jiaye <l936249247@hotmail.com>
2026-05-13 21:37:18 +0800

    启用 AIO 打包中的 ZIP 功能

--- BuildAIONoShaderTests.bat
+++ BuildAIONoShaderTests.bat
@@ -17,7 +17,7 @@ echo Configuring %PRESET% in %BUILD_DIR% for AIO package without ShaderTest...
 cmake -S . --preset="%PRESET%" -B "%BUILD_DIR%" ^
     -DBUILD_SHADER_TESTS=OFF ^
     -DZIP_TO_DIST=OFF ^
-    -DAIO_ZIP_TO_DIST=OFF ^
+    -DAIO_ZIP_TO_DIST=ON ^
     -DAUTO_PLUGIN_DEPLOYMENT=OFF
 if errorlevel 1 exit /b %ERRORLEVEL%
 
