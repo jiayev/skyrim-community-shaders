b5202b184d1dbb0d944fec4f3e384cc878fd70a5
Jiaye <l936249247@hotmail.com>
2026-05-17 15:20:04 +0800

    sky statics

--- package/Shaders/Effect.hlsl
+++ package/Shaders/Effect.hlsl
@@ -672,6 +672,11 @@ float3 GetLightingShadow(float3 color, float3 worldPosition, float2 screenPositi
 	}
 #		endif
 
+#		if defined(PHYSICAL_SKY)
+	if (SharedData::physSkyData.enabled)
+		dirColor *= PhysSky::SampleTr(normalize(SharedData::DirLightDirection.xyz), SampDepthSampler);
+#		endif
+
 	return dirColor + ambientColor;
 }
 #	endif
@@ -862,8 +867,17 @@ PS_OUTPUT main(PS_INPUT input)
 #	endif
 
 #	if !defined(LIGHTING) && defined(VC) && defined(TEXCOORD) && defined(NORMALS) && defined(TEXTURE) && defined(FALLOFF) && defined(SOFT)
-	if (Permutation::PixelShaderDescriptor & Permutation::EffectFlags::GrayscaleToAlpha && lightingInfluence == 1.0)
-		lightColor = GetLightingShadow(lightColor, input.WorldPosition.xyz, input.Position.xy, depth, eyeIndex, shadowVariance);
+	if (Permutation::PixelShaderDescriptor & Permutation::EffectFlags::GrayscaleToAlpha && lightingInfluence == 1.0) {
+#		if defined(PHYSICAL_SKY)
+		if (SharedData::physSkyData.enabled) {
+			float3 sceneLighting = ShadowSampling::GetSceneLightingColor() / (effectNormalization * Math::PI);
+			lightColor = baseColor.xyz * GetLightingShadow(sceneLighting, input.WorldPosition.xyz, input.Position.xy, depth, eyeIndex, shadowVariance);
+		} else
+#		endif
+		{
+			lightColor = GetLightingShadow(lightColor, input.WorldPosition.xyz, input.Position.xy, depth, eyeIndex, shadowVariance);
+		}
+	}
 #	endif
 
 	lightColor = Color::EffectMult(lightColor);
