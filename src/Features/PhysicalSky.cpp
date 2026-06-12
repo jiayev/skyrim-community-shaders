fcefbba80004b9b451df4104d57061b2eebe411b
Jiaye <l936249247@hotmail.com>
2026-06-12 13:55:02 +0800

    fix screensize

--- src/Features/PhysicalSky.cpp
+++ src/Features/PhysicalSky.cpp
@@ -678,7 +678,7 @@ void PhysicalSky::Reset()
 	}
 
 	// resolution
-	float2 res = globals::state->screenSize;
+	float2 res{ (float)globals::game::graphicsState->screenWidth, (float)globals::game::graphicsState->screenHeight };
 	float2 dynres = Util::ConvertToDynamic(res);
 	dynres = { floor(dynres.x), floor(dynres.y) };
 
@@ -841,7 +841,8 @@ void PhysicalSky::AccumShadow()
 	auto& terrainShadows = globals::features::terrainShadows;
 	auto& cloudShadows = globals::features::cloudShadows;
 
-	float2 size = Util::ConvertToDynamic(state->screenSize);
+	float2 screenSize{ (float)globals::game::graphicsState->screenWidth, (float)globals::game::graphicsState->screenHeight };
+	float2 size = Util::ConvertToDynamic(screenSize);
 	uint resolution[2] = { (uint)size.x, (uint)size.y };
 
 	constexpr auto debugStr = "Physical Sky: Shadow Accumulation";
