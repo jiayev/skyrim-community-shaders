c7e1ce2159f360d2cf4026ff5c02f9d3043bf86a
Gistix <gistix.md@hotmail.com>
2026-07-30 23:06:31 -0300

    better lighting defaults

--- include/CreationEngineRaytracing.h
+++ include/CreationEngineRaytracing.h
@@ -170,12 +170,12 @@ struct CreationEngineRaytracing
 
 	struct LightingSettings
 	{
-		float Directional = 1.0f;
-		float Point = 1.0f;
+		float Directional = 5.0f;
+		float Point = 5.0f;
 		bool LodDimmer = false;
-		float Emissive = 1.0f;
-		float Effect = 1.0f;
-		float Sky = 1.0f;
+		float Emissive = 5.0f;
+		float Effect = 5.0f;
+		float Sky = 5.0f;
 
 		bool operator==(const LightingSettings&) const = default;
 
