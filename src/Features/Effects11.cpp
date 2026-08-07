437dd847dd33f4026df37dc75434d8f77dc48c6a
Skrubby Skrub In A Shrub <87662196+SkrubbySkrubInAShrub@users.noreply.github.com>
2026-08-07 09:48:56 -0700

    fix(effects11): guard menus from effects (#2628)

--- src/Features/Effects11.cpp
+++ src/Features/Effects11.cpp
@@ -470,11 +470,11 @@ void Effects11::CheckCommonData()
 		ENBHelper::Update();
 
 		auto& settingManager = SettingManager::GetSingleton();
-		auto ui = globals::game::ui;
-		bool isMenuOpen = ui->IsMenuOpen(RE::MapMenu::MENU_NAME);
+		auto state = globals::state;
+		const bool inBlockedMenu = state->isMapMenuOpen || state->isStatsMenuOpen;
 		auto& effectManager = EffectManager::GetSingleton();
 
-		enableEffect = !isMenuOpen && globals::shaderCache->IsEnabled() && settingManager.GetValue<bool>("UseEffect", "GLOBAL") && effectManager.IsPresetLoaded();
+		enableEffect = !inBlockedMenu && globals::shaderCache->IsEnabled() && settingManager.GetValue<bool>("UseEffect", "GLOBAL") && effectManager.IsPresetLoaded();
 
 		auto& weatherManager = WeatherManager::GetSingleton();
 
@@ -634,7 +634,7 @@ void Effects11::DrawVolumetricRays()
 	if (globals::game::sky && globals::game::sky->flags.any(RE::Sky::Flags::kHideSky))
 		return;
 
-	if (globals::state->isMapMenuOpen)
+	if (globals::state->isMapMenuOpen || globals::state->isStatsMenuOpen)
 		return;
 
 	auto& settingManager = SettingManager::GetSingleton();
